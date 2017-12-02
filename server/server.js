ad48f4e513537a78e6e9a30b1c8cb40effc53c04
Bob Zuidema <rzuidema@us.ibm.com>
2017-12-02 16:37:26 -0500

    Created Post method

--- server/server.js
+++ server/server.js
@@ -1,4 +1,32 @@
-var {mongoose} = require('./db/mongoose'); 
+var express = require('express');
+var bodyParser = require('body-parser');
 
+var {mongoose} = require('./db/mongoose'); 
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
+var app = express();
+app.use(bodyParser.json());
+
+var PORT = 3000;
+
+app.post('/todos', (req, res) => {
+    console.log(req.body);
+    var todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then(
+        (doc) => 
+        {
+          res.send(doc);
+        }, 
+        (error) => 
+        {
+            res.status(400).send(error);
+        });
+});
+
+
+app.listen(PORT||3000, () => {
+    console.log(`Server started in port ${PORT}`);
+});
